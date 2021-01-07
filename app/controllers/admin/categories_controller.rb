efe7e78052c39a78a7c24fa13fef8e1c158aab63
Sahil Athia <sahilathia918@gmail.com>
2021-01-07 16:40:43 +0000

    add form with create method for new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -5,8 +5,24 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
   end
 end

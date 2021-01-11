2dd89716dbaa6382ce66e6eeb8dcbfd43efedaca
Sahil Athia <sahilathia918@gmail.com>
2021-01-11 21:22:35 +0000

    add a failing test for each product param

--- spec/models/product_spec_spec.rb
+++ spec/models/product_spec_spec.rb
@@ -13,15 +13,27 @@ RSpec.describe Product, type: :model do
     end
 
     it "should raise error if product doesnt have a price" do 
+      @product = Product.new({:category => @category, :name => 'desk', :price => nil, :quantity => 34})
+      expect(@product.save).to eq(false)
+      puts "\n", @product.errors.full_messages.first
     end
 
-    it "should raise error if product doesnt have a quantity" do 
+    it "should raise error if product doesnt have a quantity" do
+      @product = Product.new({:category => @category, :name => 'desk', :price => 30000, :quantity => nil})
+      expect(@product.save).to eq(false)
+      puts "\n", @product.errors.full_messages.first
     end
 
-    it "should raise error if product doesnt have a category" do 
+    it "should raise error if product doesnt have a category" do
+      @product = Product.new({:category => nil, :name => 'desk', :price => 30000, :quantity => 34})
+      expect(@product.save).to eq(false)
+      puts "\n", @product.errors.full_messages.first
     end
 
-    it "should raise error if product doesnt have a name" do 
+    it "should raise error if product doesnt have a name" do
+      @product = Product.new({:category => @category, :name => nil, :price => 30000, :quantity => 34})
+      expect(@product.save).to eq(false)
+      puts "\n", @product.errors.full_messages.first
     end
   end
 end

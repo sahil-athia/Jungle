0c9540437099ea68336a46aada221a3395640bf2
Sahil Athia <sahilathia918@gmail.com>
2021-01-11 23:26:29 +0000

    add spec for names and email prescence

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   describe 'validations' do
-  
-    it "should not pass if passwords dont match" do
-      @user = User.new({:password => "hello", :password_confirmation => "hell0", :email => "1@1", :first_name => "bob", :last_name => "jones"})
-      expect(@user.save).to eq(false)
+    it "should pass if all validations meet" do
+      @user = User.new({:password => "hello", :password_confirmation => "hello", :email => "1@1", :first_name => "bob", :last_name => "jones"})
+      expect(@user.save).to eq(true)
     end
 
     it "should not pass if password is not present" do
       @user = User.new({:password => nil, :password_confirmation => "hell0", :email => "1@1", :first_name => "bob", :last_name => "jones"})
       expect(@user.save).to eq(false)
     end
-
+    
+    it "should not pass if email is not present" do
+      @user = User.new({:password => "hell0", :password_confirmation => "hell0", :email => nil, :first_name => "bob", :last_name => "jones"})
+      expect(@user.save).to eq(false)
+    end
+    
+    it "should not pass if first name is not present" do
+      @user = User.new({:password => "hell0", :password_confirmation => "hell0", :email => "1@1", :first_name => nil, :last_name => "jones"})
+      expect(@user.save).to eq(false)
+    end
+    
+    it "should not pass if last name is not present" do
+      @user = User.new({:password => "hell0", :password_confirmation => "hell0", :email => "1@1", :first_name => "bob", :last_name => nil})
+      expect(@user.save).to eq(false)
+    end
+    
+    it "should not pass if passwords dont match" do
+      @user = User.new({:password => "hello", :password_confirmation => "hell0", :email => "1@1", :first_name => "bob", :last_name => "jones"})
+      expect(@user.save).to eq(false)
+    end
+    
     it "should not pass if emails are not unique" do
       @user1 = User.new({:password => "hell0", :password_confirmation => "hell0", :email => "hello@world.com", :first_name => "bob", :last_name => "jones"})
       @user1.save

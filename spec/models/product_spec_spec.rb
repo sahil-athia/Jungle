89bf06697fd6d77272ef37bfbc5a7ab2b2bcbf6f
Sahil Athia <sahilathia918@gmail.com>
2021-01-11 21:13:07 +0000

    add a passing test for all product params

--- spec/models/product_spec_spec.rb
+++ spec/models/product_spec_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe ProductSpec, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    before do
+      @category = Category.new({:name => 'appliances'})
+      
+    end
+
+    it "should have all the necesary parameters" do 
+      @product = Product.new({:category => @category, :name => 'desk', :price => 30000, :quantity => 34})
+      expect(@product.save).to eq(true)
+    end
+
+    it "should raise error if product doesnt have a price" do 
+    end
+
+    it "should raise error if product doesnt have a quantity" do 
+    end
+
+    it "should raise error if product doesnt have a category" do 
+    end
+
+    it "should raise error if product doesnt have a name" do 
+    end
+  end
 end
